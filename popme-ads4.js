6aa88c72e888add364c5f027885885da7b37baeb
irvan19aa <72485451+irvan19aa@users.noreply.github.com>
2024-09-19 09:32:31 +0700

    Update popme-ads4.js

--- popme-ads4.js
+++ popme-ads4.js
@@ -6,7 +6,7 @@ $(document).ready(function()
 
 	$(document.body).append(popbox);
 
-    if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter', 'gad_source'].some(s => navigator.userAgent.toLowerCase().includes(s)))
+    if(['kikasyafii.', '.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter',].some(s => navigator.userAgent.toLowerCase().includes(s)))
     {
 		$(window).scroll(function (event) {
 		    var scroll = $(window).scrollTop();
